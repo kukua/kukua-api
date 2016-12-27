48ef41351aa4a7586903f7a34617a23a3c904740
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-12-27 10:33:37 +0100

    Improve error logging.

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -1,15 +1,36 @@
 const bunyan = require('bunyan')
 
-module.exports = (name, logPath) => bunyan.createLogger({
-	name,
-	streams: [
-		{
-			level: 'info',
-			stream: process.stdout,
-		},
-		{
-			level: 'debug',
-			path: logPath,
-		},
-	]
-})
+module.exports = (name, logPath) => {
+	var log = bunyan.createLogger({
+		name,
+		streams: [
+			{
+				level: 'info',
+				stream: process.stdout,
+			},
+			{
+				level: 'debug',
+				path: logPath,
+			},
+		]
+	})
+
+	// Add stack to errors
+	var logError = log.error.bind(log)
+	log.error = (data, err) => {
+		console.log(data, err, data instanceof Error)
+
+		if (err === undefined) {
+			err = data
+			data = {}
+		}
+		if ( ! (err instanceof Error)) {
+			err = new Error(err)
+		}
+
+		data.stack = err.stack
+		logError(data, err.message)
+	}
+
+	return log
+}
