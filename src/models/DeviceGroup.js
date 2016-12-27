12308b17178cb207c853df07e2f3bc292f917cf6
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-12-27 12:49:09 +0100

    Remove device_udids from device group response.

--- src/models/DeviceGroup.js
+++ src/models/DeviceGroup.js
@@ -1,4 +1,11 @@
 const Base = require('./Base')
 
 module.exports = class DeviceGroup extends Base {
+	toJSON () {
+		var attr = Object.assign({}, this._attributes)
+
+		delete attr.device_udids
+
+		return attr
+	}
 }
