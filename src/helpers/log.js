f2696a4a985afb8f33c949d55a4b47f94cfa2595
Maurits van Mastrigt <maurits@nerdieworks.nl>
2017-01-03 17:52:53 +0100

    Fix error log method when using childs.

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -5,7 +5,22 @@ const RotatingFileStream = require('bunyan-rotating-file-stream')
 
 const logPath = String(process.env.LOG_PATH || '/tmp/output.log')
 
-var log = bunyan.createLogger({
+// Add stack to errors
+const logError = bunyan.prototype.error
+bunyan.prototype.error = function (data, err) {
+	if (err === undefined) {
+		err = data
+		data = {}
+	}
+	if ( ! (err instanceof Error)) {
+		err = new Error(err)
+	}
+
+	data.stack = err.stack
+	logError.apply(this, [data, err.message])
+}
+
+const log = bunyan.createLogger({
 	name: 'api',
 	streams: [
 		{
@@ -26,21 +41,6 @@ var log = bunyan.createLogger({
 		},
 	]
 })
-
-// Add stack to errors
-var logError = log.error.bind(log)
-log.error = (data, err) => {
-	if (err === undefined) {
-		err = data
-		data = {}
-	}
-	if ( ! (err instanceof Error)) {
-		err = new Error(err)
-	}
-
-	data.stack = err.stack
-	logError(data, err.message)
-}
 
 if (process.env.NODE_ENV !== 'production') {
 	log.level('debug')
