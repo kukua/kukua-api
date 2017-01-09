6b61a1e95fd0f6c10fee88b61a634438d1378b44
Maurits van Mastrigt <maurits@nerdieworks.nl>
2017-01-09 13:59:16 +0100

    Add job Email action.

---
+++ src/models/Job/actions/Email.js
@@ -0,0 +1,51 @@
+const Promise = require('bluebird')
+const Base = require('./Base')
+const Validator = require('../../../helpers/validator')
+const objectTemplate = require('../../../helpers/objectTemplate')
+const Mailgun = require('mailgun-js')({
+	apiKey: process.env.MAILGUN_API_KEY,
+	domain: process.env.MAILGUN_DOMAIN,
+})
+
+class Email extends Base {
+	getSchema () {
+		return {
+			from: 'required|email',
+			to: 'required|email',
+			subject: 'required|string',
+			text: 'string',
+		}
+	}
+
+	exec (data) {
+		return new Promise((resolve, reject) => {
+			var config = this.getConfig()
+			var validator = new Validator(config, this.getSchema())
+
+			if (validator.fails()) {
+				return reject('Invalid Email config: ' + validator.errors.all()[0])
+			}
+
+			var { from, to, subject, text } = config
+
+			subject = objectTemplate(subject, data)
+
+			if (text) {
+				text = objectTemplate(text, data)
+			} else if (data.transform && typeof data.transform === 'string') {
+				text = data.transform
+			} else {
+				return reject('No "text" config or "transform" string value given.')
+			}
+
+			Mailgun.messages().send({ from, to, subject, text }, (err) => {
+				if (err) return reject(err)
+				resolve()
+			})
+		})
+	}
+}
+
+Email.key = 'email'
+
+module.exports = Email
--- src/models/Job/actions/index.js
+++ src/models/Job/actions/index.js
@@ -2,5 +2,6 @@ const Transform = require('./Transform')
 const Logger = require('./Logger')
 const WebHook = require('./WebHook')
 const SlackHook = require('./SlackHook')
+const Email = require('./Email')
 
-module.exports = { Transform, Logger, WebHook, SlackHook }
+module.exports = { Transform, Logger, WebHook, SlackHook, Email }
